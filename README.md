aaae9358d674461c95ff6f5a6f431acc054bed1b
thiagoolimpio <olimpiodeoliveirathiago@gmail.com>
2025-05-02 13:49:19 -0300

    v 0.8

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ v 0.2 :
 adicionado elementos da UI 
 configurado frame corpo e top
 
-v 0.5 :
+v 0.8 :
+termino das configuracoes do frame corpo, ajustes visuais e de escrita.
+ 
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
+import requests
+
 
 ################# cor ################
 cor1 ='#444466' # preto
@@ -40,8 +43,34 @@ l_cidade = Label(frame_corpo, text='Machado - Brasil / America do Sul', anchor='
 l_cidade.place(x=10, y=4)
 
 
+l_data = Label(frame_corpo, text='09 03 2025 | 12:00:00 AM', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
+l_data.place(x=10, y=54)
 
 
+l_humidade = Label(frame_corpo, text='79', anchor='center', bg=fundo, fg=cor1, font=("Arial 45 bold"))
+l_humidade.place(x=10, y=100)
+
+l_h_simbolo = Label(frame_corpo, text='%', anchor='center', bg=fundo, fg=cor1, font=("Arial 10 bold"))
+l_h_simbolo.place(x=85, y=110)
+
+l_h_nome = Label(frame_corpo, text='Humidade', anchor='center', bg=fundo, fg=cor1, font=("Arial 8"))
+l_h_nome.place(x=85, y=140)
+
+l_pressao = Label(frame_corpo, text='Pressão : 1000', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
+l_pressao.place(x=10, y=184)
+
+l_velocidade = Label(frame_corpo, text='Velocidade do vento : 1000', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
+l_velocidade.place(x=10, y=212)
+
+imagem =  Image.open('imagens/sol.png')
+imagem = imagem.resize((130, 130))
+imagem = ImageTk.PhotoImage(imagem)
+
+l_icon = Label(frame_corpo, image=imagem, bg=fundo, font=("Arial 10"))
+l_icon.place(x=160, y=50)
+
+l_descricao = Label(frame_corpo, text='Nublado', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
+l_descricao.place(x=170, y=190)
 
 
 # estilizaçao
