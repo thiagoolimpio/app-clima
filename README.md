ff1bb46efc7d0b78039c88df458e89c8fcd46404
thiagoolimpio <olimpiodeoliveirathiago@gmail.com>
2025-05-03 08:01:45 -0300

    v 0.9

--- README.md
+++ README.md
@@ -10,5 +10,9 @@ configurado frame corpo e top
 v 0.8 :
 termino das configuracoes do frame corpo, ajustes visuais e de escrita.
  
-v 1.0 :
-criado o arquivo test para criacao do codigo para o funcionamento da API, ligacao do main com o arquivo da API
+v 0.9.0 :
+ligação com a API, ajustes para exibição das informações fornecidas.
+pequenas mudancas : agora os visores apresentam os dados recebidos corretamente.
+
+previa v 0.9.9 / 1.0
+reorganizaçao do codigo, mudanças visuais : novas cores adicionadas....
--- main.py
+++ main.py
@@ -27,6 +27,11 @@ janela.configure(bg=fundo)
 
 ttk.Separator(janela, orient=HORIZONTAL).grid(row=0, columnspan=1, padx=157)
 
+global imagem
+
+
+
+
 # criando os frames
 frame_top = Frame(janela, width=320, height=50, bg=cor2, pady=0, padx=0, )
 frame_top.grid(row=1, column=0)
@@ -48,31 +53,18 @@ l_cidade.place(x=10, y=4)
 
 # configurando frame corpo: 
 
-l_data = Label(frame_corpo, text='09 03 2025 | 12:00:00 AM', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
+l_data = Label(frame_corpo, text='', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
 l_data.place(x=10, y=54)
-
-
-l_humidade = Label(frame_corpo, text='79', anchor='center', bg=fundo, fg=cor1, font=("Arial 45 bold"))
+l_humidade = Label(frame_corpo, text='', anchor='center', bg=fundo, fg=cor1, font=("Arial 45 bold"))
 l_humidade.place(x=10, y=100)
-
-l_h_simbolo = Label(frame_corpo, text='%', anchor='center', bg=fundo, fg=cor1, font=("Arial 10 bold"))
+l_h_simbolo = Label(frame_corpo, text='', anchor='center', bg=fundo, fg=cor1, font=("Arial 10 bold"))
 l_h_simbolo.place(x=85, y=110)
-
-l_h_nome = Label(frame_corpo, text='Humidade', anchor='center', bg=fundo, fg=cor1, font=("Arial 8"))
+l_h_nome = Label(frame_corpo, text='', anchor='center', bg=fundo, fg=cor1, font=("Arial 8"))
 l_h_nome.place(x=85, y=140)
-
-l_pressao = Label(frame_corpo, text='Pressão : 1000', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
+l_pressao = Label(frame_corpo, text='', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
 l_pressao.place(x=10, y=184)
-
-l_velocidade = Label(frame_corpo, text='Velocidade do vento : 1000', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
+l_velocidade = Label(frame_corpo, text='', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
 l_velocidade.place(x=10, y=212)
-
-imagem =  Image.open('imagens/sol.png')
-imagem = imagem.resize((130, 130))
-imagem = ImageTk.PhotoImage(imagem)
-
-l_icon = Label(frame_corpo, image=imagem, bg=fundo, font=("Arial 10"))
-l_icon.place(x=160, y=50)
 
 l_descricao = Label(frame_corpo, text='Nublado', anchor='center', bg=fundo, fg=cor1, font=("Arial 10"))
 l_descricao.place(x=170, y=190)
@@ -86,24 +78,24 @@ def informacao():
     api_link = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(cidade,chave)
 
 
-    # chamando a API com o requests
+    #--------chamando a API com o requests
     r = requests.get(api_link)
 
-    # convertendo a reposta em JSON
+    #--------convertendo a reposta em JSON
     dados = r.json()
     print(dados)
     print('*'*45)
 
-    # obtendo dados pela API
+    #--------obtendo dados pela API
     pais_codigo = dados['sys']['country']
     print(pais_codigo)
 
-    #zona
+    #---------zona
     zona_fuso = pytz.country_timezones[pais_codigo]
 
     #---------país
     pais = pytz.country_names[pais_codigo]
-    #---------data----------
+    #---------data
     zona = pytz.timezone(zona_fuso[0])
     zona_horas = datetime.now(zona)
     zona_horas = zona_horas.strftime('%d %m %Y | %H:%M:%S %p')
@@ -116,7 +108,7 @@ def informacao():
     descricao = dados['weather'][0]['description']
 
     
-#mudando informacoes
+    #---------mudando informacoes
     def pais_para_continente(i):
         pais_alpha = pc.country_name_to_country_alpha2(i)
         pais_continente_codigo = pc.country_alpha2_to_continent_code(pais_alpha)
@@ -125,12 +117,42 @@ def informacao():
         return pais_continente_nome
 
     continente = pais_para_continente(pais)
-    l_cidade['text']= cidade + ' - ' + pais + ' / ' + continente
+    l_cidade['text'] = cidade + ' - ' + pais + ' / ' + continente
+    l_data['text'] = zona_horas
+    l_humidade['text'] = humidade
+    l_h_simbolo['text'] = '%'
+    l_h_nome['text'] = 'Humidade'
+    l_pressao['text'] = "Pressão: " + str(pressao)
+    l_velocidade['text'] = "Velocidade do Vento:" + str(velocidade)
+    l_descricao['text'] = descricao
 
+    #---icon
+    zona_periodo = datetime.now(zona)
+    zona_periodo =zona_periodo.strftime('%H')
+    
+    global imagem
+    zona_periodo = int(zona_periodo)
+    
+    if zona_periodo <= 5:
+        imagem =  Image.open('imagens/noite.png')
+        
+    elif zona_periodo <= 11:
+        imagem =  Image.open('imagens/sol.png')
+    else:
+        pass
+        
+        
+        
+    imagem = imagem.resize((130, 130))
+    imagem = ImageTk.PhotoImage(imagem)
 
-#passando informacoes para os respectivos campos:
-
-
+    l_icon = Label(frame_corpo, image=imagem, bg=fundo, font=("Arial 10"))
+    l_icon.place(x=162, y=50)
+    
+    
+    
+    
+#-------------botão
 b_ver = Button(frame_top, command=informacao, text='clima', bg=cor1, fg=cor3, font=("Ivy 9 bold"), relief='raised', overrelief=RIDGE,)
 b_ver.place(x=250, y=10)
 
